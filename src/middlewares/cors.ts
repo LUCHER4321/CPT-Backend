3a12b4c74e28af3c137f4432b37558e10a2cef55
LUCHER4321 <lucher4321@gmail.com>
2025-06-10 21:27:07 -0400

    Middlewares Update

---
+++ src/middlewares/cors.ts
@@ -0,0 +1,10 @@
+import cors from "cors";
+import { ACCEPTED_ORIGINS } from "../config";
+
+export const corsMw = ({ acceptedOrigins = ACCEPTED_ORIGINS }: { acceptedOrigins?: string[] } = {}) => cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(acceptedOrigins.includes(origin)) return callback(null, true);
+        return callback(new Error("CORS origin not allowed"));
+    },
+});
