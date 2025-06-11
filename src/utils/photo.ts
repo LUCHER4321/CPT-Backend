43eba155bcdf934e437e579838e98ee0b6fea62a
LUCHER4321 <lucher4321@gmail.com>
2025-06-11 18:05:44 -0400

    Update photo.ts

--- src/utils/photo.ts
+++ src/utils/photo.ts
@@ -1,4 +1,4 @@
-import { IMAGES } from "../config";
+import { BASE, IMAGES } from "../config";
 import { nullableInput } from "./nullableInput";
 
-export const photoToString = (photo: string | null | undefined) => nullableInput(photo, p => `${IMAGES}/${p}`);
+export const photoToString = (photo: string | null | undefined) => nullableInput(photo, p => `${BASE}/${IMAGES}/${p}`);
