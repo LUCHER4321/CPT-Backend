382a262471b8c9d4aa469d1fc67b5ecbc17d40bb
LUCHER4321 <lucher4321@gmail.com>
2025-06-10 21:22:27 -0400

    Create mongo.ts

---
+++ src/mongo.ts
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import { URL } from "./config";
+
+export const connectMongo = () => mongoose
+    .connect(URL)
+    .then(() => console.log("Database connected"))
+    .catch(e => console.error(e));
