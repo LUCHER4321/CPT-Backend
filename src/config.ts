42e5b3c793d7574a8243d6d795b6318c276d035a
LUCHER4321 <lucher4321@gmail.com>
2025-06-11 18:05:39 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,6 @@ export const PORT = +(process.env.PORT ?? 3000);
 export const URL = process.env.MONGO_URL ?? "";
 export const SECRET = process.env.JWT_SECRET ?? "defaultsecret";
 export const EXPIRATION = +(process.env.JWT_EXPIRATION ?? 3600);
-export const IMAGES = process.env.IMAGES_BASE_URL ?? "api/life-trees/images";
+export const IMAGES = process.env.IMAGES_URL ?? "api/life-tree/image";
+export const BASE = process.env.BASE_URL ?? `http://localhost:${PORT}`;
 export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS?.split(",") ?? [`http://localhost:${PORT}`];
