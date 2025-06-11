b0104a0d20a813767f4f4ed47be346cd8ab1a4dd
LUCHER4321 <lucher4321@gmail.com>
2025-06-11 15:26:05 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ import 'dotenv/config';
 export const PORT = +(process.env.PORT ?? 3000);
 export const URL = process.env.MONGO_URL ?? "";
 export const SECRET = process.env.JWT_SECRET ?? "defaultsecret";
-export const EXPIRATION = +(process.env.JWT_EXPIRATION ?? 3600 * 24);
-export const IMAGES = process.env.IMAGES_BASE_URL ?? `http://localhost:${PORT}/api/life-trees/images`;
+export const EXPIRATION = +(process.env.JWT_EXPIRATION ?? 3600);
+export const IMAGES = process.env.IMAGES_BASE_URL ?? "api/life-trees/images";
 export const ACCEPTED_ORIGINS = process.env.ACCEPTED_ORIGINS?.split(",") ?? [`http://localhost:${PORT}`];
