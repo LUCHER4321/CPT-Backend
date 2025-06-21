246b732ce7dcb4d0f74dc008102fad161d479268
LUCHER4321 <lucher4321@gmail.com>
2025-06-21 18:04:20 -0400

    Update image.ts

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -8,7 +8,7 @@ export const imageController = ({
     getImage: async (req, res) => {
         const { img } = req.params;
         if (!img) return res.status(400).json({ error: "Image name is required" });
-        const path = (await imageModel.getImage({ img }))?.path;
+        const { path } = await imageModel.getImage({ img }) ?? { path: undefined };
         if (!path) return res.status(404).json({ message: "Image not found" });
         res.sendFile(path);
     }
