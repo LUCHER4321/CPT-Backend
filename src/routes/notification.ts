61883107349e12fae3aaf718bb3c6c2a3953189a
LUCHER4321 <lucher4321@gmail.com>
2025-06-21 18:04:27 -0400

    Update notification.ts

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -11,7 +11,8 @@ export const createNotificationIO = ({
     io,
     notificationController
 }: IOProps) => {
-    io.on(setN + server, async(socket) => await notificationController.setNotification({ socket, call: setN + client }));
-    io.on(getN + server, async(socket) => await notificationController.getNotifications({socket, call: getN + client}));
+    const { on } = io
+    on(setN + client, async(socket) => await notificationController.setNotification({ socket, call: setN + server }));
+    on(getN + client, async(socket) => await notificationController.getNotifications({socket, call: getN + server }));
     return io;
 };
