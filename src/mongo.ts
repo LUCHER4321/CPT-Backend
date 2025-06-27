4676e15f559240869ed2337ee59670685d36d7fd
LUCHER4321 <lucher4321@gmail.com>
2025-06-26 22:44:40 -0400

    Delete mongo.ts

--- src/mongo.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import { URL } from "./config";
-
-export const connectMongo = () => mongoose
-    .connect(URL)
-    .then(() => console.log("Database connected"))
-    .catch(e => console.error(e));
