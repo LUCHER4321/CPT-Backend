e25b3853ffd9682bd67a3ad6d5e21dc6d3d5371f
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 21:12:32 -0400

    Update serverConfig.ts

--- src/server/serverConfig.ts
+++ src/server/serverConfig.ts
@@ -4,11 +4,13 @@ import { corsMw } from "../middlewares/cors";
 import cookieParser from "cookie-parser";
 import { createServer } from "node:http";
 import { Server, ServerOptions } from "socket.io";
+import { join } from "node:path";
+import { notFound } from "../middlewares/notFound";
 
 interface ServerConfigProps {
-    app?: Express;
-    webSocket?: (socket: {io: Server}) => Server;
-    baseRouter?: {
+    app: Express;
+    webSocket: (socket: { io: Server }) => Server;
+    baseRouter: {
         url: string;
         router: Router;
     };
@@ -17,20 +19,23 @@ interface ServerConfigProps {
 
 export default ({
     app,
-    baseRouter = {
-        url: "/",
-        router: Router()
+    baseRouter: {
+        url,
+        router
     },
     webSocket,
     serverOptions
-} : ServerConfigProps) => {
+}: ServerConfigProps) => {
     const server = createServer(app);
     const io = new Server(server, serverOptions);
-    const socket = webSocket?.({io});
-    app?.use(json());
-    app?.use(corsMw());
-    app?.use(morgan("dev"));
-    app?.use(cookieParser());
-    app?.use(baseRouter.url, baseRouter.router);
+    const socket = webSocket?.({ io });
+    app.disable("x-powered-by");
+    app.use(json());
+    app.use(corsMw());
+    app.use(morgan("dev"));
+    app.use(cookieParser());
+    app.get(url, (_, res) => { res.sendFile(join(__dirname, "..", "pages", "welcome.html")); })
+    app.use(url, router);
+    app.use(notFound);
     return { app, server, io, socket };
 };
