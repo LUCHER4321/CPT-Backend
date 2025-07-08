0c4d62b2f16204a659671370a35a9c7a19f4dac0
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 21:13:37 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,5 @@ const { server } = serverConfig({
 });
 
 connectMongo().then(() => server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 }));
