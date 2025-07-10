9697e72bec726ba56691433dd7cce0d9e6954b9e
LUCHER4321 <lucher4321@gmail.com>
2025-07-10 19:16:41 -0400

    Create nodemailer.ts

---
+++ src/connections/nodemailer.ts
@@ -0,0 +1,12 @@
+import { createTransport } from "nodemailer";
+import { MAILER_PASS, MAILER_USER } from "../config";
+
+export const transporter = createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: MAILER_USER,
+    pass: MAILER_PASS
+  }
+});
