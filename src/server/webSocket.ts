679c3a22643bc89dcc969bdf5a077bca97efc529
LUCHER4321 <lucher4321@gmail.com>
2025-07-12 00:38:50 -0400

    Update webSocket.ts

--- src/server/webSocket.ts
+++ src/server/webSocket.ts
@@ -2,10 +2,21 @@ import { Server } from "socket.io";
 import { createNotificationIO } from "../routes/notification";
 import { notificationController } from "../controllers/notification";
 import { notificationModel } from "../models/notification";
+import { createPhTreeIO } from "../routes/phTree";
+import { phTreeController } from "../controllers/phTree";
+import { phTreeModel } from "../models/phTree";
 
-export const webSocket = ({io}: {io: Server}) => createNotificationIO({
-    io,
-    notificationController: notificationController({
-        notificationModel
-    })
+export const webSocket = ({io}: {io: Server}) => io.on("connection", socket => {
+    createNotificationIO({
+        socket,
+        notificationController: notificationController({
+            notificationModel
+        })
+    });
+    createPhTreeIO({
+        socket,
+        phTreeController: phTreeController({
+            phTreeModel
+        })
+    });
 });
