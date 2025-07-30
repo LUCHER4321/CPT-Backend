c5a5757a3bcdd63ddbdc958c1f03bb238e0ee29f
LUCHER4321 <lucher4321@gmail.com>
2025-07-29 20:54:30 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-import { PORT } from "./config";
+import { PORT, SOCKET_PORT } from "./config";
 import { connectMongo } from "./connections/mongo";
 import { baseRouter } from "./server/baseRouter";
 import serverConfig from "./server/serverConfig";
 import express from "express";
 import { webSocket } from "./server/webSocket";
+import { corsWS } from "./middlewares/cors";
 
-const { server } = serverConfig({
+const { server, io } = serverConfig({
     app: express(),
     baseRouter,
-    webSocket
+    webSocket,
+    serverOptions: {
+        cors: corsWS()
+    }
 });
 
 connectMongo().then(() => server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    io.listen(SOCKET_PORT);
+    console.log(`Web Socket is running on http://localhost:${SOCKET_PORT}`)
 }));
