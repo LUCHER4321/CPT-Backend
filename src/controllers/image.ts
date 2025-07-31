4dd2aeab91cb47b3ce6fc513aadd3ba981d070ea
LUCHER4321 <lucher4321@gmail.com>
2025-07-31 00:27:28 -0400

    Update image.ts

--- src/controllers/image.ts
+++ src/controllers/image.ts
@@ -8,8 +8,18 @@ export const imageController = ({
     getImage: async (req, res) => {
         const { img } = req.params;
         if (!img) return res.status(400).json({ error: "Image name is required" });
-        const { path } = await imageModel.getImage({ img }) ?? { path: undefined };
+        const { path } = await imageModel.getImage({ img }) ?? {};
         if (!path) return res.status(404).json({ message: "Image not found" });
-        res.sendFile(path);
+        try {
+            const response = await fetch(path);
+            if (!response.ok) throw new Error('Failed to fetch image');
+            const contentType = response.headers.get('content-type');
+            const arrayBuffer = await response.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+            if (contentType) res.set('Content-Type', contentType);
+            res.send(buffer);
+        } catch(e) {
+            return res.status(500).json({ message: "Error fetching image" });
+        }
     }
 });
