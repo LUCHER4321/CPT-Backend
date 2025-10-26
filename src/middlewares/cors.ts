de1cfbb64cc8d4f09c761663f39f80daef107730
LUCHER4321 <lucher4321@gmail.com>
2025-10-26 14:31:55 -0300

    Update cors.ts

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,26 +1,26 @@
 import cors from "cors";
 import { ACCEPTED_ORIGINS, API } from "../config";
-import e from "cors";
 
 type StaticOrigin = boolean | string | RegExp | Array<boolean | string | RegExp>;
-
 type CustomOrigin = (
     requestOrigin: string | undefined,
     callback: (err: Error | null, origin?: StaticOrigin) => void,
 ) => void;
 
 const origin = ({ acceptedOrigins = ACCEPTED_ORIGINS }: { acceptedOrigins?: string[] } = {}): StaticOrigin | CustomOrigin | undefined => (origin, callback) => {
-    if(!origin || !API) return callback(null, true);
-    if(acceptedOrigins.includes(origin)) return callback(null, true);
+    if(!origin || API || acceptedOrigins.includes(origin)) return callback(null, true);
     return callback(new Error("CORS origin not allowed"));
 }
 
 export const corsMw = ({ acceptedOrigins = ACCEPTED_ORIGINS }: { acceptedOrigins?: string[] } = {}) => cors({
     origin: origin({ acceptedOrigins }),
-    credentials: true
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Set-Cookie'],
+    exposedHeaders: ['Set-Cookie'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 });
 
-export const corsWS = ({ acceptedOrigins = ACCEPTED_ORIGINS }: { acceptedOrigins?: string[] } = {}): e.CorsOptions | e.CorsOptionsDelegate | undefined => ({
+export const corsWS = ({ acceptedOrigins = ACCEPTED_ORIGINS }: { acceptedOrigins?: string[] } = {}): cors.CorsOptions | cors.CorsOptionsDelegate<any> => ({
     origin: origin({ acceptedOrigins }),
     credentials: true
-})
+});
