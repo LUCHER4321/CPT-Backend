6dec2ba939f39a76e94342c3b9ec4c7c56dfd6e0
LUCHER4321 <lucher4321@gmail.com>
2025-11-04 22:35:28 -0300

    Update contact.ts

--- src/routes/contact.ts
+++ src/routes/contact.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { ContactController } from "../types";
+import { allowedMethods } from "../middlewares/options";
 
 export const createContactRoutes = ({
     contactController
 }: { contactController: ContactController }) => {
     const router = Router();
     router.post("/", contactController.contact);
+    allowedMethods({ router, methods: ["POST"] });
     return router;
 }
