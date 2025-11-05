2fd3b3b45c47b9ff9bf37fa877010278d319967a
LUCHER4321 <lucher4321@gmail.com>
2025-11-05 19:38:23 -0300

    Update nodemailer.ts

--- src/connections/nodemailer.ts
+++ src/connections/nodemailer.ts
@@ -1,22 +1,16 @@
 import { createTransport } from "nodemailer";
-import { API, MAILER_PASS, MAILER_USER } from "../config";
+import { MAILER_PASS, MAILER_USER } from "../config";
 
-const timeout = 3 * 60 * 1000;
+const timeout = 30 * 1000;
 
 export const transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: API ? 465 : 587,
-  secure: !!API, // true for 465, false for other ports
+  service: "gmail",
   auth: {
     user: MAILER_USER,
     pass: MAILER_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  },
   connectionTimeout: timeout,
-  socketTimeout: timeout,
-  greetingTimeout: timeout
+  socketTimeout: timeout
 });
 
 transporter.verify()
